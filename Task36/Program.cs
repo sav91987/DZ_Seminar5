71ed8acdb8053a347b06216b8df09e88ce27a184
Sotnikov_Andrey <sav91987@ya.ru>
2023-02-26 13:21:33 +0300

    Added Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,30 @@
+﻿System.Console.WriteLine("Задайте число элементов массива");
+int length = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Задайте нижнюю границу диапазона чисел массива:");
+int minValue = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Задайте верхнюю границу диапазона чисел массива:");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[] GetArray(int size, int minValueMeth, int maxValueMeth)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValueMeth, maxValueMeth + 1);
+    }
+    return result;
+}
+
+int[] array = GetArray(length, minValue, maxValue);
+int sum = 0;
+
+for (int i = 1; i < length; i += 2)
+{
+    sum += array[i];
+}
+
+System.Console.WriteLine($"[{String.Join("; ", array)}]");
+System.Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sum}");
