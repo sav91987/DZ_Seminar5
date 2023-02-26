eac240e6c7bbbf47ef96a5284524c9d29c4fd6c8
Sotnikov_Andrey <sav91987@ya.ru>
2023-02-26 14:05:10 +0300

    Added Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,31 @@
+﻿System.Console.WriteLine("Задайте число элементов массива");
+int length = Convert.ToInt32(Console.ReadLine());
+
+double[] GetArray(int size)
+{
+    double[] result = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = Math.Round(new Random().NextDouble() * 100, 2);
+    }
+    return result;
+}
+
+double[] array = GetArray(length);
+
+double sum = GetMaxMin(length, array);
+double GetMaxMin(int size, double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] > max) max = array[i];
+        else if (array[i] < min) min = array[i];
+    }
+    return max + min;
+}
+
+System.Console.WriteLine($"[{String.Join("; ", array)}]");
+System.Console.WriteLine($"Сумма максимального и минимального элементов равна: {sum}");
